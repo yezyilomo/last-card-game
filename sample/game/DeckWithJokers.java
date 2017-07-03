a5e31c7f2149d1d9e6c3340c21601c5c6fc059ad
yezy <yezileliilomo@hotmail.com>
2017-07-04 02:11:45 +0300

    UI update

--- sample/game/DeckWithJokers.java
+++
@@ -1,9 +0,0 @@
-package sample.game;
-
-public class DeckWithJokers extends DeckWithoutJokers{
-  public DeckWithJokers(){
-    super();
-    deck.push(new Card(14,"NONE"));
-    deck.push(new Card(15,"NONE"));
-  }
-}
